a20a60efb6f535d23ae2af28bb5683c778ec33cb
GMIT-mmurray <michael.murray@gmit.ie>
2017-02-03 10:06:42 +0000

    #3

--- GroupA/sketch_feb03a/sketch_feb03a.ino
+++ GroupA/sketch_feb03a/sketch_feb03a.ino
@@ -1,4 +1,4 @@
-int timer = 100;           // The higher the number, the slower the timing.
+// The higher the number, the slower the timing.
 
 void setup() {
   // use a for loop to initialize each pin as an output:
@@ -11,19 +11,21 @@ void loop() {
   // loop from the lowest pin to the highest:
   for (int thisPin = 2; thisPin < 10; thisPin++) {
     // turn the pin on/off:
-    blinkLED(thisPin);
+    long r = random(1000, 3001);
+    blinkLED(thisPin, r);
   }
 
   // loop from the highest pin to the lowest:
   for (int thisPin = 9; thisPin >= 2; thisPin--) {
     // turn the pin on/off:
-    blinkLED(thisPin);
+    long r = random(1000, 3001);
+    blinkLED(thisPin, r);
   }
 }
 
-void blinkLED(int pin) {
+void blinkLED(int pin, int t) {
   digitalWrite(pin, HIGH);
-  delay(timer);
+  delay(t);
   // turn the pin off:
   digitalWrite(pin, LOW);
 }
