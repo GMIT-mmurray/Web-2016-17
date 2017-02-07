8f2c1d87c31cfbcb87a96e280821ac4de8704bc3
GMIT-mmurray <michael.murray@gmit.ie>
2017-02-07 16:43:06 +0000

    #1

---
+++ GroupB/sketch_feb07a/sketch_feb07a.ino
@@ -0,0 +1,31 @@
+
+int timer = 1000;           // The higher the number, the slower the timing.
+int ledPins[] = {3,7,4,2,6,5};
+int pinCount = 6;
+
+void setup() {
+  // use a for loop to initialize each pin as an output:
+  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
+    pinMode(ledPins[thisPin], OUTPUT);
+  }
+}
+
+void loop() {
+  // loop from the lowest pin to the highest:
+  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
+    // turn the pin on:
+    digitalWrite(ledPins[thisPin], HIGH);
+    delay(timer);
+    // turn the pin off:
+    digitalWrite(ledPins[thisPin], LOW);
+  }
+
+  // loop from the highest pin to the lowest:
+  for (int thisPin = pinCount-1; thisPin >= 0; thisPin--) {
+    // turn the pin on:
+    digitalWrite(ledPins[thisPin], HIGH);
+    delay(timer);
+    // turn the pin off:
+    digitalWrite(ledPins[thisPin], LOW);
+  }
+}
