8d8dd96f16069bc8fd5eff5b181f16e57f5f6c20
GMIT-mmurray <michael.murray@gmit.ie>
2017-02-14 17:13:27 +0000

    #3

--- GroupB/sketch_feb14a/sketch_feb14a.ino
+++ GroupB/sketch_feb14a/sketch_feb14a.ino
@@ -1,46 +1,60 @@
 
 int timer = 1000;           // The higher the number, the slower the timing.
-int ledPins1[] =    {2, 2}; //Enable Motor 1 //Enable Motor 1
-int ledPins2[] =    {3, 4}; // Forward 1     // Rev
-int ledPins3[] =    {5, 5}; //Enable Motor 2 //Enable Motor 2
-int ledPins4[] =    {6, 7}; // Forward 2     // Rev
-int blinkRates[] =  {1000, 2000};
-int pinCount = 2;
-
+int ledPins1[] =    {2, 2, 2}; //Enable Motor 1 //Enable Motor 1
+int ledPins2[] =    {3, 4, 4}; // Forward 1     // Rev
+int ledPins3[] =    {5, 5, 0}; //Enable Motor 2 //Enable Motor 2
+int ledPins4[] =    {6, 7, 0}; // Forward 2     // Rev
+int blinkRates[] =  {5000, 2000, 1000};
+int pinCount = 3;
+int startSwitch = 8;
+int switchState = 0;
+int motorSwitchingTime = 300;
 void setup() {
   // use a for loop to initialize each pin as an output:
-  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
-    pinMode(ledPins1[thisPin], OUTPUT);
-    pinMode(ledPins2[thisPin], OUTPUT);
-    pinMode(ledPins3[thisPin], OUTPUT);
-    pinMode(ledPins4[thisPin], OUTPUT);
+  pinMode(startSwitch, INPUT);
+  for (int thisPin = 2; thisPin < 8; thisPin++) {
+    pinMode(thisPin, OUTPUT);
   }
 }
 
 void loop() {
   // loop from the lowest pin to the highest:
-  for (int thisPin = 0; thisPin < pinCount; thisPin++) {
-    // turn the pin on:
-    digitalWrite(ledPins1[thisPin], HIGH);
-    digitalWrite(ledPins2[thisPin], HIGH);
-    digitalWrite(ledPins3[thisPin], HIGH);
-    digitalWrite(ledPins4[thisPin], HIGH);
-    delay(blinkRates[thisPin]);
-    digitalWrite(ledPins1[thisPin], LOW);
-    digitalWrite(ledPins2[thisPin], LOW);
-    digitalWrite(ledPins3[thisPin], LOW);
-    digitalWrite(ledPins4[thisPin], LOW);
-    delay(blinkRates[thisPin]);
+  switchState = digitalRead(startSwitch);
+  if (switchState == HIGH) {
+    for (int thisPin = 0; thisPin < pinCount; thisPin++) {
+      // turn the pin on:
+      if (ledPins1[thisPin] != 0) {
+        digitalWrite(ledPins1[thisPin], HIGH);
+      }
+      if (ledPins2[thisPin] != 0) {
+        digitalWrite(ledPins2[thisPin], HIGH);
+      }
+      if (ledPins3[thisPin] != 0) {
+        digitalWrite(ledPins3[thisPin], HIGH);
+      }
+      if (ledPins4[thisPin] != 0) {
+        digitalWrite(ledPins4[thisPin], HIGH);
+      }
+
+      delay(blinkRates[thisPin]);
+      if (ledPins1[thisPin] != 0) {
+        digitalWrite(ledPins1[thisPin], LOW);
+      }
+      if (ledPins2[thisPin] != 0) {
+        digitalWrite(ledPins2[thisPin], LOW);
+      }
+      if (ledPins3[thisPin] != 0) {
+        digitalWrite(ledPins3[thisPin], LOW);
+      }
+      if (ledPins4[thisPin] != 0) {
+        digitalWrite(ledPins4[thisPin], LOW);
+      }
+      delay(motorSwitchingTime);
+    }
   }
 }
 
 
-void blinkLed( int pin, int t) {
-  digitalWrite(pin, HIGH);
-  delay(t);
-  // turn the pin off:
-  digitalWrite(pin, LOW);
-  delay(t);
-}
+
 
 
