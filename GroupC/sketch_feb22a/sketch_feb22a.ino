ada07bc029c2856cf4778c2bcd6152bdf4c584ce
GMIT-mmurray <michael.murray@gmit.ie>
2017-02-22 15:07:05 +0000

    #2

--- GroupC/sketch_feb22a/sketch_feb22a.ino
+++ GroupC/sketch_feb22a/sketch_feb22a.ino
@@ -1,16 +1,24 @@
 const int  buttonPin = 2;    // the pin that the pushbutton is attached to
 const int ledPin = 13;       // the pin that the LED is attached to
-
+const int  buttonPin1 = 3;    // the pin that the pushbutton is attached to
+const int ledPin1 = 12;       // the pin that the LED is attached to
 // Variables will change:
 int buttonPushCounter = 0;   // counter for the number of button presses
 int buttonState = 0;         // current state of the button
 int lastButtonState = 0;     // previous state of the button
+// Pin 3
+int buttonPushCounter1 = 0;   // counter for the number of button presses
+int buttonState1 = 1;         // current state of the button
+int lastButtonState1 = 1;     // previous state of the button
+
 
 void setup() {
   // initialize the button pin as a input:
   pinMode(buttonPin, INPUT);
+  pinMode(buttonPin1, INPUT);
   // initialize the LED as an output:
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
   // initialize serial communication:
   Serial.begin(9600);
 }
@@ -19,7 +27,8 @@ void setup() {
 void loop() {
   // read the pushbutton input pin:
   buttonState = digitalRead(buttonPin);
-
+  buttonState1 = digitalRead(buttonPin1);
+  
   // compare the buttonState to its previous state
   if (buttonState != lastButtonState) {
     // if the state has changed, increment the counter
@@ -27,13 +36,31 @@ void loop() {
       // if the current state is HIGH then the button
       // wend from off to on:
       buttonPushCounter++;
-      Serial.println("on");
-      Serial.print("number of button pushes:  ");
+      Serial.println("Pin 2 Pos Transition");
+      Serial.print("number of button pushes on Pin 2 :  ");
       Serial.println(buttonPushCounter);
     } else {
       // if the current state is LOW then the button
       // wend from on to off:
-      Serial.println("off");
+      Serial.println("Pin 2 Neg Transition");
+    }
+    // Delay a little bit to avoid bouncing
+    delay(50);
+  }
+
+    if (buttonState1 != lastButtonState1) {
+    // if the state has changed, increment the counter
+    if (buttonState1 == LOW) {
+      // if the current state is HIGH then the button
+      // wend from off to on:
+      buttonPushCounter1++;
+      Serial.println(" Pin 3 Neg Transition");
+      Serial.print("number of button pushes on Pin 3:  ");
+      Serial.println(buttonPushCounter1);
+    } else {
+      // if the current state is LOW then the button
+      // wend from on to off:
+      Serial.println("Pin 3 Pos Transition");
     }
     // Delay a little bit to avoid bouncing
     delay(50);
@@ -41,7 +68,7 @@ void loop() {
   // save the current state as the last state,
   //for next time through the loop
   lastButtonState = buttonState;
-
+  lastButtonState1 = buttonState1;
 
   // turns on the LED every four button pushes by
   // checking the modulo of the button push counter.
@@ -52,6 +79,10 @@ void loop() {
   } else {
     digitalWrite(ledPin, LOW);
   }
-
+  if (buttonPushCounter1 % 4 == 0) {
+    digitalWrite(ledPin, LOW);
+  } else {
+    digitalWrite(ledPin, HIGH);
+  }
 }
 
